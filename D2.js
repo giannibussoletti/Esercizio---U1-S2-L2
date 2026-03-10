e3d869541a53012a6a1de75c883fecb46df2e2dc
Gianni <BussolettiGianni@gmail.com>
2026-03-10 15:30:20 +0100

    Update D2.js

--- D2.js
+++ D2.js
@@ -100,17 +100,51 @@ if (promo20 >= prezzoMinimo) {
 
 /* SCRIVI QUI LA TUA RISPOSTA */
 
+const gg = 7
+const hh = 30
+const ii = -1
+
+if (gg > hh && gg > ii) {
+  console.log(gg + " > " + hh + " > " + ii)
+} else if (gg > ii && gg > hh) {
+  console.log(gg + " > " + ii + ">" + hh)
+} else if (hh > gg && hh > ii) {
+  console.log(hh + " > " + gg + " > " + ii)
+} else if (hh > ii && hh > gg) {
+  console.log(hh + " > " + ii + " > " + gg)
+} else if (ii > gg && ii > hh) {
+  console.log(ii + " > " + gg + " > " + hh)
+} else {
+  console.log(ii + " > " + hh + " > " + gg)
+}
+
 /* ESERCIZIO 8
   Crea un algoritmo per verificare che un valore fornito sia un numero oppure no (suggerimento: cerca su un motore di ricerca "typeof").
 */
 
 /* SCRIVI QUI LA TUA RISPOSTA */
 
+let isItNumber = 4
+
+if (typeof isItNumber === "number") {
+  console.log("complimenti!")
+} else {
+  console.log("Oh no")
+}
+
 /* ESERCIZIO 9
   Crea un algoritmo per controllare se un numero fornito sia pari o dispari (suggerimento: cerca l'operatore modulo su un motore di ricerca)
 */
 
+const opModulo = 20
+const zeroResto = opModulo % 2
+
 /* SCRIVI QUI LA TUA RISPOSTA */
+if (opModulo % 2 === 0) {
+  console.log("Il numero è divisibile per due, il suo resto è " + zeroResto)
+} else {
+  console.log("Il numero non è divisibile per due, il suo resto è " + zeroResto)
+}
 
 /* ESERCIZIO 10
   Modifica la logica del seguente algoritmo in modo che mostri in console il messaggio corretto in ogni circostanza.
@@ -126,17 +160,28 @@ if (promo20 >= prezzoMinimo) {
 
 /* SCRIVI QUI LA TUA RISPOSTA */
 
+let val = 10
+if (val < 10 && val >= 5) {
+  console.log("Meno di 10")
+} else if (val < 5) {
+  console.log("Meno di 5")
+} else {
+  console.log("Uguale a 10 o maggiore")
+}
+
 /* ESERCIZIO 11
   Fornito il seguente oggetto, scrivi del codice per aggiungere una proprietà "city", il cui valore sarà "Toronto".
 */
 
+/* SCRIVI QUI LA TUA RISPOSTA */
 const me = {
   name: "John",
   lastName: "Doe",
   skills: ["javascript", "html", "css"],
 }
 
-/* SCRIVI QUI LA TUA RISPOSTA */
+me.city = "Toronto"
+console.log(me)
 
 /* ESERCIZIO 12
   Lavorando sempre sull'oggetto precedentemente fornito, scrivi del codice per rimuovere la proprietà "lastName".
@@ -144,20 +189,33 @@ const me = {
 
 /* SCRIVI QUI LA TUA RISPOSTA */
 
+delete me.lastName
+console.log(me)
+
 /* ESERCIZIO 13
   Lavorando sempre sull'oggetto precedentemente fornito, scrivi del codice per rimuovere l'ultimo elemento della proprietà "skills".
 */
 
 /* SCRIVI QUI LA TUA RISPOSTA */
 
+me.skills.pop()
+console.log(me)
+
 /* ESERCIZIO 14
   Scrivi del codice per creare un array inizialmente vuoto. Riempilo successivamente con i numeri da 1 a 10.
 */
 
 /* SCRIVI QUI LA TUA RISPOSTA */
+arrayNum = []
+
+arrayNum.splice(0, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10)
+console.log(arrayNum)
 
 /* ESERCIZIO 15
   Scrivi del codice per sostituire l'ultimo elemento dell'array, ovvero il valore 10, con il valore 100.
 */
 
 /* SCRIVI QUI LA TUA RISPOSTA */
+
+arrayNum.splice(length - 1, 1, 100)
+console.log(arrayNum)
